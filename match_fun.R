3894515ff3e462ed8bfcb9353f6135b9a6bd510c
kevjosey <kjosey@hsph.harvard.edu>
2022-03-05 17:19:27 -0500

    Dimb mistake.

--- match_fun.R
+++ match_fun.R
@@ -43,7 +43,7 @@ match_estimate <- function(a, w, x, zip, a.vals, fmla, trim = 0.01, attempts = 5
   # marginalize
   estimate <- sapply(a.vals, function(a.tmp, ...) {
     
-    match_estimate <- predict(match_curve, newdata = data.frame(a = a.tmp, covar), type = "response")
+    match_estimate <- predict(match_curve, newdata = data.frame(pm25 = a.tmp, covar), type = "response")
     return(weighted.mean(match_estimate, w = wts, na.rm = TRUE))
     
   })
