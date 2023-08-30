b2c5d3725c3ef96ba0b6634aa1f1c90c81bb41b4
kevjosey <kevin.josey@cuanschutz.edu>
2023-08-30 15:31:01 -0600

    Making gam function for more general.

--- Functions/gam.R
+++ Functions/gam.R
@@ -1,6 +1,6 @@
-## Spline estimate
-gam_est <- function(a, psi, a.vals = seq(min(a), max(a), length.out = 100),
-                    weights = NULL, se.fit = FALSE, sandwich = FALSE,
+## GAM Estimation of the ERCs
+gam_est <- function(a, psi, family = gaussian(), weights = NULL, 
+                    a.vals = seq(min(a), max(a), length.out = 100), se.fit = FALSE,
                     astar = NULL, astar2 = NULL, x = NULL, cmat = NULL, ipw = NULL) {
   
   n <- length(a)
@@ -8,16 +8,18 @@ gam_est <- function(a, psi, a.vals = seq(min(a), max(a), length.out = 100),
   if (is.null(weights))
     weights <- rep(1, times = length(a))
   
-  # Natural Cubic Spline
-  mod <- gam(psi ~ s(a), weights = weights, family = quasipoisson())
-  g.std <- predict(mod, type = "lpmatrix")
+  # GAM Models
+  mod <- gam(psi ~ s(a), weights = weights, family = family)
+  
+  g <- predict(mod, type = "lpmatrix")
+  mu <- predict(mod, type = "response")
   g.vals <- predict(mod, type = "lpmatrix", newdata = data.frame(a = a.vals))
-  mu <- mod$family$linkinv(c(g.vals %*% mod$coefficients))
+  mu.vals <- predict(mod, type = "response", newdata = data.frame(a = a.vals))
   
   if (se.fit) {
     
     m <- ncol(cmat)
-    l <- ncol(g.std)
+    l <- ncol(g)
     U <- matrix(0, ncol = m, nrow = m)
     V <- matrix(0, ncol = m + l, nrow = l)
     meat <- matrix(0, ncol = m + l, nrow = m + l)
@@ -27,7 +29,7 @@ gam_est <- function(a, psi, a.vals = seq(min(a), max(a), length.out = 100),
       
       U[1:m,1:m] <- U[1:m,1:m] - ipw[i]*tcrossprod(cmat[i,])
       V[,1:m] <- V[,1:m] - weights[i]*psi[i]*tcrossprod(g.std[i,],cmat[i,])
-      V[,(m + 1):(m + l)] <- V[,(m + 1):(m + l)] - weights[i]*eta[i]*tcrossprod(g.std[i,])
+      V[,(m + 1):(m + l)] <- V[,(m + 1):(m + l)] - weights[i]*family$mu.eta(family$linkfun(mu))*tcrossprod(g.std[i,])
       
       meat <- meat + 
         tcrossprod(esteq_gam(p = ipw[i], x = x[i,], psi = psi[i],
@@ -51,7 +53,7 @@ gam_est <- function(a, psi, a.vals = seq(min(a), max(a), length.out = 100),
       
       Sig <- bread %*% meat %*% t(bread)
       BV <- Sig[(m + 1):(m + l),(m + 1):(m + l)]
-      variance <- diag((mu*g.vals) %*% BV %*% t(mu*g.vals))
+      variance <- diag((family$mu.eta(family$linkfun(mu.vals))*g.vals) %*% BV %*% t(family$mu.eta(family$linkfun(mu.vals))*g.vals))
       
     }
     
@@ -67,7 +69,7 @@ esteq_gam <- function(p, x, psi, g.std, weights, astar, astar2, eta) {
   eq1 <- p*x*astar
   eq2 <- p*astar2
   eq3 <- p*x - x
-  eq4 <- weights*(psi - eta)*g.std
+  eq4 <- weights*(psi - eta)*g
   
   eq <- c(eq1, eq2, eq3, eq4) 
   return(eq)
